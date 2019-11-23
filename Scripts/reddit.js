790d2bf13d9d4fe0b16702a8eee19cf7fe1ef08b
Ricardo Graça <44160998+DisturbeD-347939@users.noreply.github.com>
2019-11-23 23:34:49 +0000

    Created a function to check format

--- Scripts/reddit.js
+++ Scripts/reddit.js
@@ -5,44 +5,41 @@ var download = require('./downloadURL.js');
 var fs = require('fs');
 var rimraf = require('rimraf');
 
-module.exports =
+//Check format
+function checkFormat(posts, i, callback)
 {
-    //Download and organize reddit posts from r/evilbuildings
-    collect: function(reddit, posts_per_day, postNumber)
+    var fileFormat = "";
+    for(var j = posts[i].url.length - 1; j > 0; j--)
     {
         reddit.getSubreddit('evilbuildings', posts_per_day).getHot().then(posts => 
         {
-            for(var i = 1; i < posts_per_day; i++)
-            {
-                console.log("Creating post " + postNumber);
-    
-                //Create post folder
-                fs.mkdirSync('./Posts/' + postNumber);
-                if(fs.existsSync('./Posts/' + postNumber))
-                {
-                    //Add the title to a txt file
-                    fs.writeFileSync('./Posts/' + postNumber + '/title.txt', posts[i].title);
-                }
-    
-                //Check file format
-                var fileFormat = "";
-                for(var j = posts[i].url.length - 1; j > 0; j--)
-                {
-                    if(posts[i].url[j] != ".")
-                    {
-                        fileFormat += posts[i].url[j];
-                    }
-                    else 
-                    {
-                        break;
-                    }
-                }
-    
-                //Reverse string
-                fileFormat = fileFormat.split("");
-                fileFormat = fileFormat.reverse();
-                fileFormat = fileFormat.join("");
-                console.log(fileFormat);
+            fileFormat += posts[i].url[j];
+        }
+        else 
+        {
+            break;
+        }
+    }
+
+    //Reverse string
+    fileFormat = fileFormat.split("");
+    fileFormat = fileFormat.reverse();
+    fileFormat = fileFormat.join("");
+
+    //Check file extension
+    if(fileFormat.length == 3)
+    {
+        callback([1, fileFormat]);
+    }
+    else 
+    {
+        rimraf('./Posts/' + postNumber, function () 
+        {
+            console.log('Directory ' + postNumber + " deleted!"); 
+            callback([0, ""]);
+        });
+    }
+}
 
                 //Download photo
                 if(fileFormat.length == 3)
