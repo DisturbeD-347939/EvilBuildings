5531ad061c3881765e4cc02f081904b87cadc845
Ricardo Graça <44160998+DisturbeD-347939@users.noreply.github.com>
2019-11-23 01:16:11 +0000

    Added external function

--- Scripts/setup.js
+++ Scripts/setup.js
@@ -1,3 +1,7 @@
+//File management
+var fs = require('fs');
+const csv = require('csv-parser');
+
 function createDir(path)
 {
     if(!fs.existsSync(path))
@@ -39,3 +43,51 @@ function readFile(path, callback)
         });
     }
 }
+
+module.exports = 
+{
+    //Setting up the program to run
+    run: function(callback)
+    {
+        //Variables
+        var postNumber, keyLocation, countries = [];
+        check = [0,0,0];
+
+        //Check for the existence of the necessary folders
+        createDir('./Posts');
+        createDir('./Used');
+
+        //Check for the existence of the necessary files
+        createFile('./Posts/Counter.txt', 0);
+
+        //Read txt files
+        readFile('./Posts/Counter.txt', function(data)
+        {
+            postNumber = data;
+            check[0] = 1;
+            console.log("Counter replied with " + postNumber);
+        });
+        readFile('path.txt', function(data)
+        {
+            keyLocation = data;
+            check[1] = 1;
+            console.log("Key replied with " + keyLocation);
+        });
+
+        //Read csv files
+        fs.createReadStream('countries_data.csv').pipe(csv()).on('data', (row) => 
+        {
+            countries.push([row.Country, row.Name]);
+        }).on('end', () => 
+        {
+            console.log("Countries retrieved!");
+            check[2] = 1;
+        });
+
+        if(check == [1,1,1])
+        {
+            callback(postNumber, keyLocation, countries);
+        }        
+    }
+}
+
