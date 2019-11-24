8c4fc534d40b737e222910b51a3c43c1638aebce
Ricardo Graça <44160998+DisturbeD-347939@users.noreply.github.com>
2019-11-24 00:30:02 +0000

    Function to prepare tweets before posting

--- Scripts/twitter.js
+++ Scripts/twitter.js
@@ -1,3 +1,24 @@
 //File management
 var fs = require('fs');
 var fsE = require('fs-extra');
+
+//Post photo and title to twitter
+function prepare(callback)
+{
+    //Check path
+    var path = __dirname.split("\\");
+    path.splice(-1, path.length-1);
+    path = path.join("/");
+
+    //Check which one is the oldest post and collect its image and title
+    var posts = fs.readdirSync(path + '/Posts');
+    var post = fs.readdirSync(path + '/Posts/' + posts[0]);
+    var image = fs.readFileSync(path + "/Posts/" + posts[0] + "/" + post[0], { encoding: 'base64' });
+
+    //Read title
+    fs.readFile(path + '/Posts/' + posts[0] + '/' + post[1], 'utf8', function(err, data) 
+    {
+        callback([data, image, posts[0]]);
+    });
+}
+
