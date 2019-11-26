667fc9dd142c96e3c67cabdc014c383879e70dee
Ricardo Graça <44160998+DisturbeD-347939@users.noreply.github.com>
2019-11-26 11:33:49 +0000

    Auth and necessary modules

--- Scripts/imageRecognition.js
+++ Scripts/imageRecognition.js
@@ -1,3 +1,30 @@
+//File management
+const fs = require('fs');
+
+//IBM Visual Recognition module
+const VisualRecognitionV3 = require('watson-developer-cloud/visual-recognition/v3');
+
+function auth(callback)
+{
+    //Get files
+    var configFile = fs.readFileSync('./config.json');
+    var configData = JSON.parse(configFile);
+    var keyPath = configData.config[0].keys_path;
+    var credentials = fs.readFileSync(keyPath, 'utf-8');
+    var parsedCredentials = JSON.parse(credentials);
+
+    //Authentication into the Personality Insight API
+    var visualRecognition = new VisualRecognitionV3
+    ({
+        version: parsedCredentials.ibm[1].version,
+        iam_apikey: parsedCredentials.ibm[1].iam_apikey,
+        url: parsedCredentials.ibm[1].url,
+    });
+    callback(visualRecognition);
+}
+
+
+
 module.exports =
 {
     classify: function(path, callback)
